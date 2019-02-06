5e06518195a48109b010f2263a32c46011cc6e7c
Zaki Shaikh <zaki-shaikh@outlook.com>
2019-02-06 23:35:25 +0530

    Polyline added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,18 @@ export class AppComponent {
     { lat: 25.5941, lng: 85.1376 },
     { lat: 26.9124, lng: 75.7873 }
   ];
+  polylines = [
+    { lat: 12.9716, lng: 77.5946 },    
+    { lat: 13.0827, lng: 80.2707 },
+    { lat: 17.3850, lng: 78.4867 },
+    { lat: 19.0760, lng: 72.8777 },
+    { lat: 22.5726, lng: 88.3639 },
+    { lat: 25.5941, lng: 85.1376 },
+    { lat: 26.9124, lng: 75.7873 },
+    { lat: 27.1767, lng: 78.0081 },
+    { lat: 28.7041, lng: 77.1025 },
+    { lat: 30.7333, lng: 76.7794 }    
+  ];
   previousWindow:AgmInfoWindow = null;
 
   clickMarker(infoWindow:AgmInfoWindow){    
